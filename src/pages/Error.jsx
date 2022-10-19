88c3f967e3d45c3d09b8ba49c4dbbdb944dee33f
SGdevWeb <samuel.gustin.dev@gmail.com>
2022-10-19 17:08:26 +0200

    Error page created with responsive design

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,14 @@
+import './Error.css';
+import { Link } from 'react-router-dom';
+
 function ErrorPage() {
-    return (<div>
-        <h1>erreur 404</h1>
+    return (
+    <div className="errorPageContainer">
+        <h1>404</h1>
+        <p>
+            Oups! La page que vous demandez n'existe pas.
+        </p>
+        <Link className='homeLink' to="/">Retourner sur la page d'accueil</Link>
     </div>
     )
 }
